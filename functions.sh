6d8528c11be95559fe7a0e68bd03f1da9100512b
Wendly Saintil <wspecs@hotmail.com>
2020-07-20 18:23:48 -0700

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -309,3 +309,12 @@ backup_file() {
   fi
 }
 
+function join() {
+    # $1 is return variable name
+    # $2 is sep
+    # $3... are the elements to join
+    local retname=$1 sep=$2 ret=$3
+    shift 3 || shift $(($#))
+    printf -v "$retname" "%s" "$ret${@/#/$sep}"
+}
+
